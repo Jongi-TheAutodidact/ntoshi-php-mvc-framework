019c01ce491fa75cb2bc0eb66cdb60cf8c9bf874
unknown <TechKaffir>
2025-06-04 03:34:03 +0200

    Updated the config file

--- app/core/config.php
+++ app/core/config.php
@@ -6,7 +6,7 @@ defined('ROOTPATH') OR exit('Access Denied!');
 if(empty($_SERVER['SERVER_NAME']) && php_sapi_name() == 'cli' || (!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost'))
 {
 	/** database config **/
-	define('DBNAME', 'ntoshi_frame_db');
+	define('DBNAME', '');
 	define('DBHOST', 'localhost'); 
 	define('DBUSER', 'root');
 	define('DBPASS', '');
